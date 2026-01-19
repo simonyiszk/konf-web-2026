29cef8e587e80f0c5f012cce4f064770158c453f
Benedek Szabó <58933758+SzBeni2003@users.noreply.github.com>
2026-01-19 19:00:29 +0100

    Added date, title next to logo, changed Vine height (#7)

--- src/components/navbar/navbar-items.tsx
+++ src/components/navbar/navbar-items.tsx
@@ -41,7 +41,7 @@ const links = [
 
 export function NavbarItems() {
   return (
-    <div className="flex flex-col md:flex-row gap-3 mt-5 md:mt-0 md:gap-10">
+    <div className="flex flex-col items-center md:flex-row gap-3 mt-5 md:mt-0 md:gap-10">
       {links.map(({ href, label }) => (
         <Link
           href={href}
