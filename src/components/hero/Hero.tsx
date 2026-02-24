1d8291e9724ae9d97fd5b66e73d7b5498e302732
Szabó Benedek <benedek.szabo2003@gmail.com>
2026-02-24 12:41:19 +0100

    hero countdown

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -5,7 +5,7 @@ import CountdownTileImplementation from "@/components/tiles/countdown-tile/count
 export default function Hero() {
   return (
     <section className="w-full bg-background">
-      <div className="flex flex-col justify-center w-full overflow-hidden pt-8 sm:pt-12 md:pt-16 gap-4 sm:gap-6">
+      <div className="flex flex-col justify-center w-full overflow-hidden pt-3 md:pt-8 gap-4 sm:gap-6">
         <HeroLogo className="w-[320px] sm:w-[520px] md:w-[620px] h-auto self-center" />
         <div className="flex flex-col md:grid md:grid-cols-2">
           <House className="flex flex-col h-auto" />
