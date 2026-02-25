6e2d19b45a1b058ed5acc7c39d192a483c07d74e
Szabó Benedek <benedek.szabo2003@gmail.com>
2026-02-25 12:05:38 +0100

    changed contact page grid for small viewports

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -29,7 +29,7 @@ export default async function asyncContact() {
               <OrganiserTile key={organiser.emailAddress} {...organiser} />
             ))}
           </div>*/}
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {sortedOrganizers.map((organiser) => (
               <OrganiserTile key={organiser.emailAddress} {...organiser} />
             ))}
